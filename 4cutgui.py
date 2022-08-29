3e2ba72e28fef15be8dd79c36ea9c1d288b58524
taegukang35 <chrsitianous@gmail.com>
2022-08-29 23:15:51 +0900

    SLIDE 1~3 complete, slide 4 need to develop

--- 4cutgui.py
+++ 4cutgui.py
@@ -1,65 +1,168 @@
 import pygame
-
+import pygame.camera
+import pygame.image
+import cv2
+import time
 # initialize game
 pygame.init()
 
 # screen option setting
-size = [1600,900]
+size = [1920, 1080]
+icon = pygame.image.load('gui_imgs/SADA_logo.png')
 screen = pygame.display.set_mode(size)
+pygame.display.set_icon(icon)
 pygame.display.set_caption('사다네컷')
 
 # game setting
 clock = pygame.time.Clock()
 
-class obj:
+class Obj:
     def __init__(self):
         self.x = 0
         self.y = 0
         self.sx = 0
         self.sy = 0
-    def put_img(self,img_path):
+
+    def put_img(self, img_path):
         if img_path[-3:] == 'png':
             self.img = pygame.image.load(img_path).convert_alpha()
         else:
             self.img = pygame.image.load(img_path)
         self.sx, self.sy = self.img.get_size()
-    def change_size(self,sx,sy):
-        self.img = pygame.transform.scale(self.img,(sx,sy))
-        self.sx,self.sy = self.img.get_size()
+
+    def change_size(self, sx, sy):
+        self.img = pygame.transform.scale(self.img, (sx, sy))
+        self.sx, self.sy = self.img.get_size()
+
     def show(self):
-        screen.blit(self.img,(self.x,self.y))
+        screen.blit(self.img, (self.x, self.y))
 
-# test object
-test = obj()
-test.put_img('gui_imgs/test.jpg')
-test.change_size(200,200)
-test.x,test.y = 0,0
 
-# phhot1 object
-img1 = obj()
-img1.put_img('gui_imgs/test.jpg')
-img1.x,img1.y = 500,0
+# object setting
 
-# main event
+slide1 = Obj()
+slide1.put_img('gui_imgs/슬라이드1.png')
+slide1.change_size(1920, 1080)
+slide1.x, slide1.y = 0, 0
+
+slide2 = Obj()
+slide2.put_img('gui_imgs/슬라이드2.png')
+slide2.change_size(1920, 1080)
+slide2.x, slide2.y = 0, 0
+
+slide3 = Obj()
+slide3.put_img('gui_imgs/슬라이드3.png')
+slide3.change_size(1920, 1080)
+slide3.x, slide3.y = 0, 0
+
+slide4 = Obj()
+slide4.put_img('gui_imgs/슬라이드4.png')
+slide4.change_size(1920, 1080)
+slide4.x, slide4.y = 0, 0
+
+slide5 = Obj()
+slide5.put_img('gui_imgs/슬라이드5.png')
+slide5.change_size(1920, 1080)
+slide5.x, slide5.y = 0, 0
+
+slide6 = Obj()
+slide6.put_img('gui_imgs/슬라이드6.png')
+slide6.change_size(1920, 1080)
+slide6.x, slide6.y = 0, 0
+
+# check img
+check1 = Obj()
+check1.put_img('gui_imgs/check.png')
+check1.change_size(250, 250)
+
+check2 = Obj()
+check2.put_img('gui_imgs/check.png')
+check2.change_size(250, 250)
+
+bool1, bool2 = False, False
+
+# slide event, frame
 SB = 0
+SN = 1
+slide1.show()
+FRAME_NUM = 0
+PEOPLE_NUM = 0
+mx, my = 0, 0
+
+# camera setting
+pygame.camera.init()
+cameras = pygame.camera.list_cameras()
+webcam = pygame.camera.Camera(cameras[0])
+webcam.start()
 
 while SB == 0:
     # FPS setting
-    clock.tick(1)
-    #screen.fill((255, 255, 255))
-
+    pygame.time.delay(10)
     # update
-    pygame.display.flip()
-
-    # sense inputs()
+    #pygame.display.flip()
+    pygame.display.update()
+    # sense inputs
     for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.QUIT:
-            SB = 1
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mx, my = pygame.mouse.get_pos()
-                print(mx,my)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mx, my = pygame.mouse.get_pos()
+            print(mx,my)
+    if SN == 1:
+        slide1.show()
+        if 768 < mx < 1153 and 641 < my < 760:
+            SN = 2
+    elif SN == 2:
+        slide2.show()
+        # check1
+        if bool1 == False:
+            if 189 < mx < 453 and 178 < my < 577:
+                FRAME_NUM = 1
+                check1.x, check1.y = 194, 263
+                bool1 = True
+            elif 348 < mx < 768 and 178 < my < 577:
+                FRAME_NUM = 2
+                check1.x, check1.y = 505, 263
+                bool1 = True
+            elif 807 < mx < 1077 and 178 < my < 577:
+                FRAME_NUM = 3
+                check1.x, check1.y = 830, 263
+                bool1 = True
+            elif 1118 < mx < 1387 and 178 < my < 577:
+                FRAME_NUM = 4
+                check1.x, check1.y = 1130, 263
+                bool1 = True
+            elif 1432 < mx < 1697 and 178 < my < 577:
+                FRAME_NUM = 5
+                check1.x, check1.y = 1440, 263
+                bool1 = True
+        # check2
+        if bool2 == False:
+            if 605 < mx < 814 and 752 < my < 964:
+                PEOPLE_NUM = 2
+                check2.x, check2.y = 610,724
+                bool2 = True
+            elif 1104 < mx < 1316 and 753 < my < 963:
+                PEOPLE_NUM = 4
+                check2.x, check2.y = 1100,724
+                bool2 = True
+        if bool1: check1.show()
+        if bool2: check2.show()
+        if bool1 and bool2:
+            SN = 3
+            #pygame.time.delay(500)
+    elif SN==3:
+        slide3.show()
+        pygame.time.delay(3000)
+        SN = 4
 
-    test.show()
-    pygame.display.flip()
+    elif SN==4:
+        # opencv로 이미지 찍게 하자 이거는 pygame 이용
+        slide4.show()
+        img = webcam.get_image()
+        img = pygame.transform.flip(img, True, False)
+        # draw frame
+        screen.blit(pygame.transform.scale(img, (640*1.8, 480*1.8)), (381, 190))
+        # screen.blit(overlay, (258, 178))
+        pygame.display.flip()
+        # grab next frame
+
 pygame.quit()
--- main.py
+++ main.py
@@ -1,59 +1,30 @@
-import cv2
-import glob
-from qr import *
+import pygame.camera
+import pygame.image
+import sys
 
-def makeframe(frame_path):
-    # 프레임 이미지 불러오기
-    frame = cv2.imread(frame_path)
-    frame = cv2.resize(frame, dsize=(756,1134), interpolation=cv2.INTER_AREA)
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+pygame.camera.init()
+cameras = pygame.camera.list_cameras()
+webcam = pygame.camera.Camera(cameras[0])
+webcam.start()
 
-    # 프레임 박스 정보
-    left_boxes = {
-        'box1': [(16, 10), (356, 239)],
-        'box2': [(16, 257), (356, 484)],
-        'box3': [(16, 498), (356, 727)],
-        'box4': [(16, 744), (356, 972)]
-    }
+# grab first frame
+img = webcam.get_image()
 
-    right_boxes = {
-        'box5': [(393, 10), (735, 239)],
-        'box6': [(393, 257), (735, 484)],
-        'box7': [(393, 498), (735, 727)],
-        'box8': [(393, 744), (735, 972)]
-    }
+WIDTH = img.get_width()
+HEIGHT = img.get_height()
 
-    # 프레임 위에 이미지 넣기
-    for (filename, left_box, right_box) in zip(glob.iglob('photos/*.jpg', recursive=True), left_boxes.values(), right_boxes.values()):
-        img = cv2.imread(filename)
-        #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        lp1, lp2 = left_box
-        rp1, rp2 = right_box
-        roi = cv2.resize(img, dsize=(lp2[0] - lp1[0], lp2[1] - lp1[1]), interpolation=cv2.INTER_AREA)
-        frame[lp1[1]+3:lp2[1]+3, lp1[0]+3:lp2[0]+3] = roi
-        roi = cv2.resize(img, dsize=(rp2[0] - rp1[0], rp2[1] - rp1[1]), interpolation=cv2.INTER_AREA)
-        frame[rp1[1]+3:rp2[1]+3, rp1[0]+3:rp2[0]+3] = roi
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("pyGame Camera View")
 
-    # QR 코드 생성
-    eigen = getMD5(str(time.time()))[:10]
-    urltoQR('qrqr.png','http://sada.dothome.co.kr/photo/'+eigen+'.png')
-    qrimg = cv2.imread('qrqr.png')
-    w,h,_ = qrimg.shape
-    qrimg = qrimg[35:w-35,35:h-35]
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            sys.exit()
 
-    # 프레임에 QR코드 넣기
-    lp1,lp2 = [(289, 1045), (356, 1113)]
-    rp1,rp2 = [(669, 1047), (735, 1113)]
-    roi = cv2.resize(qrimg, dsize=(lp2[0] - lp1[0], lp2[1] - lp1[1]), interpolation=cv2.INTER_AREA)
-    frame[lp1[1]+2:lp2[1]+2, lp1[0]+1:lp2[0]+1] = roi
-    roi = cv2.resize(qrimg, dsize=(rp2[0] - rp1[0], rp2[1] - rp1[1]), interpolation=cv2.INTER_AREA)
-    frame[rp1[1]+1:rp2[1]+1, rp1[0]:rp2[0]] = roi
-
-    return frame
-
-# 모든 프레임에 대해 이미지 넣고 결과 저장하기
-i=1
-for frame in glob.iglob('frames/*.png', recursive=True):
-    result = makeframe(frame)
-    cv2.imwrite('results/result'+str(i)+'.png', result)
-    i += 1
+    # draw frame
+    screen.blit(img, (0, 0))
+    print(img.get_width(),img.get_height())
+    #screen.blit(overlay, (258, 178))
+    pygame.display.flip()
+    # grab next frame
+    img = webcam.get_image()
